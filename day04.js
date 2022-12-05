236b935354e6f1b6bf12cd28310e77b88fbe5a11
Matt DuLeone <matt@duleone.com>
2022-12-04 22:28:56 -0500

    Tidy day 4

--- day04.js
+++ day04.js
@@ -2,36 +2,18 @@ const { data } = require("./data/day04");
 
 const parsePairs = (ranges) => {
   const [first, second] = ranges.split(",");
-  const [ff, fs] = first.split("-");
-  const [sf, ss] = second.split("-");
 
   return {
-    first: [Number(ff), Number(fs)],
-    second: [Number(sf), Number(ss)],
+    first: first.split("-").map(Number),
+    second: second.split("-").map(Number),
   };
 };
 
-const findWholeOverlap = ({
-  first: [startF, endF],
-  second: [startS, endS],
-}) => {
-  if (
-    (startF <= startS && endF >= endS) ||
-    (startS <= startF && endS >= endF)
-  ) {
-    return true;
-  }
+const findWholeOverlap = ({ first: [startF, endF], second: [startS, endS] }) =>
+  (startF <= startS && endF >= endS) || (startS <= startF && endS >= endF);
 
-  return false;
-};
-
-const findOverlap = ({ first: [startF, endF], second: [startS, endS] }) => {
-  if (endF < startS || endS < startF) {
-    return false;
-  }
-
-  return true;
-};
+const findOverlap = ({ first: [startF, endF], second: [startS, endS] }) =>
+  endF >= startS && endS >= startF;
 
 console.log(
   data
